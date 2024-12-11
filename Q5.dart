1057c921c53813fb13f244c2f9db5a48afad6815
HaseebAhmed08 <ha9719121@gmail.com>
2024-12-12 03:27:45 +0500

    adding new files

---
+++ Q5.dart
@@ -0,0 +1,15 @@
+// void main() { 
+  
+//   Map<String, String> name = {
+//  "Ali": "05784", "Ahmed": "6485", "Jameel": "3646", "Bilal": "78569" };
+//   List<String> homeNumbers = name.keys.where((key) => key.length == 4).toList(); 
+
+// print(homeNumbers); 
+
+// }
+
+
+
+
+
+// Q5,Q10,Q11,Q13
--- Q9.dart
+++ Q9.dart
@@ -2,8 +2,6 @@ void main() {
   List numbers = [12, 3, 6, 58, 64, 66, 47, 35];
 
   int greatest = numbers.reduce((a, b) => a > b ? a : b);
-  int smallest = numbers.reduce((a, b) => a < b ? a : b);
 
   print("Greatest number: $greatest");
-  print("Smallest number: $smallest");
 }
