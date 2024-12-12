947798536096472c481d821aeda36e5029e1b326
HaseebAhmed08 <ha9719121@gmail.com>
2024-12-12 16:35:54 +0500

    i have commented in  Q4

--- Q4.dart
+++ Q4.dart
@@ -1,4 +1,8 @@
 void main() {
+  
+//   Q.4: Create a list of numbers & write a program to get the smallest & greatest
+//   number from a list.
+
   List numbers = [12, 3, 6, 58, 64, 66, 47, 35];
 
   int greatest = numbers.reduce((a, b) => a > b ? a : b);
