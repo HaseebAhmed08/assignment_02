09d0b64afca53bf2cdd421bc83ccb4f970c4c3b1
HaseebAhmed08 <ha9719121@gmail.com>
2024-12-13 21:03:02 +0500

    i have added comments about code

--- Q2.dart
+++ Q2.dart
@@ -2,8 +2,11 @@ void main() {
 //     Q.2: Create an empty list of type string called days. Use the add method to add
 //     names of 7 days and print all days.
 
+
+// empty List
   List<String> days = [];
 
+// adding all name of days by using add method of List
   days.addAll([
     "Saturday",
     "Sunday",
@@ -13,5 +16,7 @@ void main() {
     "Thrusday",
     "Friday"
   ]);
+
+  // Reuslt
   print(days);
 }
